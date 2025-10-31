56974d05eb89c54c284df23f470e8c2028afe8c2
liuzhe <1804319025@qq.com>
2025-10-31 16:40:08 +0800

    fix

--- src/templates/Category.vue
+++ src/templates/Category.vue
@@ -1,13 +1,33 @@
 <template>
-    <Header>
-        <div>分类</div>
-    </Header>
+  <Header>
+    <div class="section-header">
+      <h2 class="section-title">{{ title }}</h2>
+    </div>
+  </Header>
 </template>
 
 <script>
+import { tags } from "@/data.js";
+
 export default {
-    mounted(){
-        console.log(this.$route.params)
-    }
-}
+  data() {
+    return {
+      title: tags[this.$route.params.tag],
+    };
+  },
+};
 </script>
+
+<style scoped>
+.section-header {
+  text-align: center;
+  margin-bottom: 40px;
+}
+
+.section-title {
+  font-size: 32px;
+  font-weight: 700;
+  margin-bottom: 10px;
+  color: #fff;
+}
+</style>
