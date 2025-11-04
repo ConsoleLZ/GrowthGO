5124f64c29a31f568bde9ef1e2a06c1c44e659a5
liuzhe <1804319025@qq.com>
2025-11-04 17:35:57 +0800

    feat

--- src/pages/Statistics.vue
+++ src/pages/Statistics.vue
@@ -20,7 +20,12 @@
         <div>
           <el-statistic title="本站访问量">
             <template #formatter>
-              <span v-loading="isLoadingVisit" element-loading-spinner="el-icon-loading" style="left: -8px;" id="busuanzi_value_site_pv"></span>
+              <span
+                v-loading="isLoadingVisit"
+                element-loading-spinner="el-icon-loading"
+                style="left: -8px;"
+                id="busuanzi_value_site_pv"
+              ></span>
             </template>
           </el-statistic>
         </div>
@@ -35,6 +40,14 @@
         </div>
       </el-col>
     </el-row>
+
+    <div
+      style="width: 100vw;display: flex;justify-content: center;align-items: center;margin-top: 30px;"
+    >
+      <div class="charts">
+        <canvas id="myChart" width="400" height="400"></canvas>
+      </div>
+    </div>
   </Header>
 </template>
 
@@ -48,6 +61,7 @@ query {
 
 <script>
 import { tags, mainData } from "@/data.js";
+import Chart from "chart.js";
 
 export default {
   data() {
@@ -55,48 +69,98 @@ export default {
       tagLength: Object.keys(tags).length,
       allLength: mainData.length,
       isLoadingVisit: true,
-      runtimeText: '0秒',
-      timer: null
+      runtimeText: "0秒",
+      timer: null,
     };
   },
+  methods: {
+    // 不蒜子脚本加载
+    loadBusuanzi() {
+      this.isLoadingVisit = true;
+      const script = document.createElement("script");
+      script.src = "//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js";
+      script.async = true;
+
+      script.onload = () => {
+        this.isLoadingVisit = false;
+      };
+
+      document.head.appendChild(script);
+    },
+    // 本站运行时间的计算
+    calculateOnlineTime() {
+      const startTime = new Date(this.$static.metadata.onlineTime).getTime();
+
+      const updateRuntime = () => {
+        const now = new Date().getTime();
+        const diff = now - startTime;
+
+        if (diff < 0) {
+          this.runtimeText = "尚未开始";
+          return;
+        }
+
+        const seconds = Math.floor(diff / 1000) % 60;
+        const minutes = Math.floor(diff / 60000) % 60;
+        const hours = Math.floor(diff / 3600000) % 24;
+        const days = Math.floor(diff / 86400000);
+
+        this.runtimeText = `${days}天${hours}小时${minutes}分钟${seconds}秒`;
+      };
+
+      updateRuntime();
+      this.timer = setInterval(updateRuntime, 1000);
+    },
+    // 生成统计饼图
+    createCharts() {
+      var ctx = document.getElementById("myChart");
+      var myChart = new Chart(ctx, {
+        type: "bar",
+        data: {
+          labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+          datasets: [
+            {
+              label: "# of Votes",
+              data: [12, 19, 3, 5, 2, 3],
+              backgroundColor: [
+                "rgba(255, 99, 132, 0.2)",
+                "rgba(54, 162, 235, 0.2)",
+                "rgba(255, 206, 86, 0.2)",
+                "rgba(75, 192, 192, 0.2)",
+                "rgba(153, 102, 255, 0.2)",
+                "rgba(255, 159, 64, 0.2)",
+              ],
+              borderColor: [
+                "rgba(255,99,132,1)",
+                "rgba(54, 162, 235, 1)",
+                "rgba(255, 206, 86, 1)",
+                "rgba(75, 192, 192, 1)",
+                "rgba(153, 102, 255, 1)",
+                "rgba(255, 159, 64, 1)",
+              ],
+              borderWidth: 1,
+            },
+          ],
+        },
+      });
+    },
+  },
   mounted() {
-    this.isLoadingVisit = true
-    const script = document.createElement('script');
-    script.src = '//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js';
-    script.async = true;
-
-    script.onload = () => {
-      this.isLoadingVisit = false;
-    };
-
-    document.head.appendChild(script);
-
-    const startTime = new Date(this.$static.metadata.onlineTime).getTime();
-
-    const updateRuntime = () => {
-      const now = new Date().getTime();
-      const diff = now - startTime;
-
-      if (diff < 0) {
-        this.runtimeText = "尚未开始";
-        return;
-      }
-
-      const seconds = Math.floor(diff / 1000) % 60;
-      const minutes = Math.floor(diff / 60000) % 60;
-      const hours = Math.floor(diff / 3600000) % 24;
-      const days = Math.floor(diff / 86400000);
-
-      this.runtimeText = `${days}天${hours}小时${minutes}分钟${seconds}秒`;
-    };
-
-    updateRuntime();
-    this.timer = setInterval(updateRuntime, 1000);
+    this.loadBusuanzi();
+    this.calculateOnlineTime();
+    this.createCharts();
   },
   beforeUnmount() {
     if (this.timer) {
       clearInterval(this.timer);
     }
-  }
+  },
 };
 </script>
+
+<style scoped>
+.charts {
+  width: 500px;
+  height: 420px;
+}
+</style>
