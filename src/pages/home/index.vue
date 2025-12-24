00fa459641f9396ef5ca1b96d453aeae31b2ef45
liuzhe <1804319025@qq.com>
2025-12-24 09:00:28 +0800

    feat

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,7 +1,19 @@
 <template>
-	<div>首页
-		<px-button>Primary</px-button>
+	<div class="header">
+		<px-avatar :size="40">
+			<px-image src="/favicon.ico" alt="Image"></px-image>
+		</px-avatar>
+		<div class="menu">
+			<div>首页</div>
+			<div>推荐</div>
+			<div>标签</div>
+		</div>
+
+		<px-input placeholder="Please input">
+			<template #prefix>$</template>
+		</px-input>
 	</div>
 </template>
 
 <script lang="ts" src="./index.ts"></script>
+<style src="./index.css" scoped></style>
